7faad06c375305ed0ee81c44cc5c0cdbd2770028
komodoSam <shastri.jr@gmail.com>
2024-11-20 21:52:00 -0600

    player controls + gravity

--- main.py
+++ main.py
@@ -1,57 +1,136 @@
 import pygame
 pygame.init()
+from PIL import Image
 vec = pygame.math.Vector2     #sets up vectors for later physics (Gravity and friction stuff)
 clock = pygame.time.Clock()
 
-accel = 0.5
-friction = -0.1
 
 screen = pygame.display.set_mode((1280,720))   #creates window
 pygame.display.set_caption("2D Game")          #sets up screen size and captions it
+background = pygame.image.load("CombinedBlue.png").convert()
+background = pygame.transform.scale(background, (1280, 720))
 
+objects = []
 
 PInputR = 0
 PInputL = 0
 PInputU = 0    #initializes the booleans for player input
 PInputD = 0
-P_PositionX = 640    #starts player in middle of the screen (for now)
-P_PositionY = 360
-PlayerVelocity = [0, 0]   #velocity vector, using a list
+GameOver = False
+HighSchore = 0
 
-Dumbass = pygame.image.load('SteamMan.png').convert()    #imports sprite for character
-screen.blit(Dumbass,(P_PositionX,P_PositionY))
+class Object:
+    def __init__(self, x, y, width, height, image):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height     #Creates object class for us to use when making player sprite
+        self.image = image
+        self.velocity = [0, 0]
 
-def check_keys(key, value):
-    R = L = U = D = 0
-    if key == pygame.K_RIGHT:
-        R = value
-    elif key == pygame.K_LEFT:
-        L = value
-    elif key == pygame.K_UP:
-        U = value
-    elif key == pygame.K_DOWN:
-        D = value
-    return R, L, U, D
+        objects.append(self)   #adds new objects to object list
 
-while True:
+        self.direction = 0
+        self.flipX = False
+        self.frame = 0
+        self.spritelist = ["SteamWalk1.png", "SteamWalk2.png", "SteamWalk3.png", "SteamWalk4.png", "SteamWalk5.png", "SteamWalk6.png"]
+        self.frame_timer = 0
 
-    screen.fill((0, 0, 240))   #refreshes background before player movement
+    def change_direction(self):
+        if self.velocity[0] < 0:      #boolean for which direction character is facing
+            self.flipX = True
+        elif self.velocity[0] > 0:
+            self.flipX = False
+    
+    def set_velocity(self, xr, xl, yu, yd):
+        self.velocity[0] = (xr - xl) * 3
+        self.velocity[1] = (yd - yu) * 3
+    
+    def draw_self(self):
+        sprite = pygame.image.load(self.spritelist[self.frame])      #loads image from animation sprite list
+        image = pygame.transform.scale(sprite, (self.width, self.height))
+
+        self.change_direction()   #updates which direction player's facing
+        
+        image = pygame.transform.flip(image, self.flipX, False)    #flips the image if in left direction
+        screen.blit(image, (self.x, self.y)) 
+
+        if self.velocity[0] == 0:
+            self.frame = 0    #If the player isn't moving set character to first sprit (resting image)
+            return
+        
+        self.frame_timer += 1   
+
+        if self.frame_timer < 10:   #basically the frame rate for the animation 
+            return
+        self.frame += 1
+        if self.frame >= len(self.spritelist):    #restarts the animation if the cycle is complete
+            self.frame = 0
+
+        self.frame_timer = 0
+
+    def gravity(self):
+        #If player not touching cloud, gravity pulls character down
+
+        #If player falls to bottom, stop their fall + KILL YOURSELF
+        if self.y < 700:
+            self.velocity[1] += 3
+        else:
+            GameOver = True
+
+
+    def update(self):
+        self.set_velocity(PInputR, PInputL, PInputU, PInputD)
+        self.gravity()  #calls gravity before updating
+        self.x += self.velocity[0]
+        self.y += self.velocity[1]   #updates player position + redraws character
+        self.draw_self()
+
+
+class Player(Object):
+    def __init__(self, x, y, width, height, image):
+        super().__init__(x, y, width, height, image)   #Player class, made from Object class (being buggy rn)
+
+
+#objects
+
+Sam = Player(640, 100, 31, 41, "SteamMan.png")
+
+
+#test_entity = Entity(600, 600, 50, 50, "SteamMan_walk.png", 5)
+
+
+while GameOver == False:
+    screen.blit(background, (0, 0))   #refreshes background before player movement
 
     for event in pygame.event.get():
+        # Key up or down
         if event.type == pygame.QUIT:  #closes window, if user exits game
             pygame.quit()
             SystemExit()
-        elif event.type == pygame.KEYDOWN:
-            PInputR, PInputL, PInputU, PInputD = check_keys(event.key, 1)
-        elif event.type == pygame.KEYUP:
-            PInputR, PInputL, PInputU, PInputD = check_keys(event.key, 0)
-    
+        elif event.type == pygame.KEYDOWN:   #If user presses down on key, acknowledges the input
+            if event.key == pygame.K_RIGHT:
+                PInputR = 1
+            elif event.key == pygame.K_LEFT:
+                PInputL = 1
+            elif event.key == pygame.K_UP:
+                PInputU = 1
+            elif event.key == pygame.K_DOWN:
+                PInputD = 1
+        elif event.type == pygame.KEYUP:     #If user lets go of key, stops player movement in that direction
+            if event.key == pygame.K_RIGHT:
+                PInputR = 0
+            elif event.key == pygame.K_LEFT:
+                PInputL = 0
+            elif event.key == pygame.K_UP:
+                PInputU = 0
+            elif event.key == pygame.K_DOWN:
+                PInputD = 0
 
-    P_PositionX += (PInputR - PInputL) * 5
-    P_PositionY += (PInputD - PInputU) * 5
+    Player.update(Sam)      #calls Player then updates them + draws
 
-
-    pygame.draw.rect(screen, (0, 0, 0), (P_PositionX, P_PositionY, 100, 50)) #starts the character in middle of screen
 
     clock.tick(60)    #caps it from refreshing more than 60 times a second
     pygame.display.update()    #updates changes in sprites
+
+print("Game Over")
